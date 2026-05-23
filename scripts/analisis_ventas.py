c43738c3da7c0bc8328f9fcd08c2a6e1dd7fdecb
Santino Maire <santinoutn17@gmail.com>
2026-05-23 20:37:03 +0000

    PROY-2: Agrego las metricas y sus resultados, tambien convierto las fechas para su uso en el grafico.

--- scripts/analisis_ventas.py
+++ scripts/analisis_ventas.py
@@ -1,4 +1,4 @@
-#Importo una libreria especializada en la manipulación y análisis de datos
+
 import pandas as pd
 
 #Lee dataset
@@ -6,3 +6,18 @@ df = pd.read_csv("datos/dataset.csv")
 
 #Muestra las primeras filas
 print(df.head())
+
+#Calcula las métricas
+ventas_totales = df["sales_amount"].sum()
+promedio_ventas = df["sales_amount"].mean()
+venta_maxima = df["sales_amount"].max()
+venta_minima = df["sales_amount"].min()
+
+#Muestra los resultados
+print("Ventas totales:", ventas_totales)
+print("Promedio:", promedio_ventas)
+print("Venta máxima:", venta_maxima)
+print("Venta mínima:", venta_minima)
+
+#Convierte las fechas
+df["sales_date"] = pd.to_datetime(df["sales_date"])
